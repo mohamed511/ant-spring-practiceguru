84abef2bfd1f8f54f4dab8b66649c946ec4783c0
mohamed511 <mohamedsayed194@hotmail.com>
2024-05-15 21:22:28 +0300

    initialization

---
+++ src/main/java/com/ant/springpracticeguru/AntSpringPracticeguruApplication.java
@@ -0,0 +1,13 @@
+package com.ant.springpracticeguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntSpringPracticeguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntSpringPracticeguruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ant/springpracticeguru/AntSpringPracticeguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ant.springpracticeguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntSpringPracticeguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
