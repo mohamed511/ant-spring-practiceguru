c6e4b9e921f6932218d70c7d70bbb282037f9846
mohamed511 <mohamedsayed194@hotmail.com>
2024-08-28 09:20:13 +0300

    add unit test for validation

--- src/main/java/com/ant/springpracticeguru/entities/Product.java
+++ src/main/java/com/ant/springpracticeguru/entities/Product.java
@@ -2,6 +2,8 @@ package com.ant.springpracticeguru.entities;
 
 import com.ant.springpracticeguru.domain.ProductStyle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -22,12 +24,24 @@ public class Product {
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
+
     @Version
     private Integer version;
+
+    @NotBlank
+    @NotNull
     private String productName;
+
+    @NotNull
     private ProductStyle productStyle;
+
+    @NotBlank
+    @NotNull
     private String upc;
+
     private Integer quantityOnHand;
+
+    @NotNull
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
--- src/test/java/com/ant/springpracticeguru/controller/ProductControllerTest.java
+++ src/test/java/com/ant/springpracticeguru/controller/ProductControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.HashMap;
 import java.util.Map;
